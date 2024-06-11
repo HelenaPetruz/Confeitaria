9a6d183329bb141eaabf9cd339af702998037676
HelenaPetruz <cl203253@g.unicamp.br>
2024-06-11 18:12:50 -0300

    Dependências adicionadas

---
+++ src/test/java/com/mentoria/helena/confeitaria/ConfeitariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentoria.helena.confeitaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfeitariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
