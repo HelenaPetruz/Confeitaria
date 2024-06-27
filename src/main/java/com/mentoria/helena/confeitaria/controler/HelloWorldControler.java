0d53f52dde0e99d1dd22f1dd64de7b58e4b89ed2
HelenaPetruz <cl203253@g.unicamp.br>
2024-06-27 20:02:20 -0300

    Hello world - aprendi sobre anotações

---
+++ src/main/java/com/mentoria/helena/confeitaria/controler/HelloWorldControler.java
@@ -0,0 +1,16 @@
+package com.mentoria.helena.confeitaria.controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ola-mundo")
+public class HelloWorldControler {
+    @GetMapping
+    public String helloWorld(){
+        String hello;
+        hello="Hello world!";
+        return hello;
+    }
+}
