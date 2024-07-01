09d33f9281a78357c83aea7a060d18445d9ba863
HelenaPetruz <cl203253@g.unicamp.br>
2024-07-01 07:43:19 -0300

    FuncionarioController - exibição da lista de funcionarios

--- src/main/java/com/mentoria/helena/confeitaria/classes/Funcionario.java
+++ src/main/java/com/mentoria/helena/confeitaria/classes/Funcionario.java
@@ -10,13 +10,21 @@ public class Funcionario extends Pessoa {
         this.idFuncionario = id;
     }
 
-    public void exibir() {
-        System.out.printf("========== FUNCIONARIO " + idFuncionario +" ==========\n");
-        System.out.println("Nome:" + nome + "\n");
-        System.out.println("Telefone:" + telefone + "\n");
-        System.out.println("Cpf:" + cpf + "\n");
-        System.out.println("Idede:" + idade + "\n");
-        System.out.println("Salario:" + salario + "\n");
+    public String exibir() {
+        String retorno="";
+        String titulo = "========== FUNCIONARIO " + idFuncionario +" ==========<br>";
+        retorno=retorno.concat(titulo);
+        String nomeC = "Nome: "+ nome+ "<br>";
+        retorno=retorno.concat(nomeC);
+        String telefoneC = "Telefone: "+ telefone + "<br>";
+        retorno=retorno.concat(telefoneC);
+        String cpfC = "Cpf: "+cpf + "<br>";
+        retorno=retorno.concat(cpfC);
+        String idadeC = "Idade: "+idade+ "<br>";
+        retorno=retorno.concat(idadeC);
+        String salarioC = "Salario: "+ salario+ "<br>";
+        retorno=retorno.concat(salarioC);
+        return retorno;
     }
 
     public Funcionario(String nome, int idade, String telefone, String cpf, double salario) {
---
+++ src/main/java/com/mentoria/helena/confeitaria/controler/FuncionarioController.java
@@ -0,0 +1,75 @@
+package com.mentoria.helena.confeitaria.controler;
+
+import com.mentoria.helena.confeitaria.classes.Cliente;
+import com.mentoria.helena.confeitaria.classes.Funcionario;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+@RestController
+@RequestMapping("/funcionarios-da-confeitaria")
+public class FuncionarioController {
+
+    ArrayList<Funcionario> listaFuncionario = new ArrayList();
+    public void inicializar(){
+        Funcionario f1 = new Funcionario();
+        f1.setNome("Pedro Leoni");
+        f1.setIdade(46);
+        f1.setTelefone("9999999999");
+        f1.setCpf("44444444444");
+        f1.setSalario(2000);
+        Funcionario f2 = new Funcionario();
+        f2.setNome("Maria");
+        f2.setIdade(35);
+        f2.setTelefone("9999999999");
+        f2.setCpf("44444444444");
+        f2.setSalario(2000);
+        Funcionario f3 = new Funcionario();
+        f3.setNome("Vanessa");
+        f3.setIdade(21);
+        f3.setTelefone("9999999999");
+        f3.setCpf("44444444444");
+        f3.setSalario(2000);
+        Funcionario f4 = new Funcionario();
+        f4.setNome("Ricardo");
+        f4.setIdade(67);
+        f4.setTelefone("9999999999");
+        f4.setCpf("44444444444");
+        f4.setSalario(2000);
+        Funcionario f5 = new Funcionario();
+        f5.setNome("Manuela");
+        f5.setIdade(23);
+        f5.setTelefone("9999999999");
+        f5.setCpf("44444444444");
+        f5.setSalario(2000);
+        Funcionario f6 = new Funcionario();
+        f6.setNome("Joaquim");
+        f6.setIdade(80);
+        f6.setTelefone("9999999999");
+        f6.setCpf("44444444444");
+        f6.setSalario(2000);
+
+        listaFuncionario.add(f1);
+        listaFuncionario.add(f2);
+        listaFuncionario.add(f3);
+        listaFuncionario.add(f4);
+        listaFuncionario.add(f5);
+        listaFuncionario.add(f6);
+    }
+
+    public FuncionarioController() {
+        inicializar();
+    }
+
+    @GetMapping
+    public String exibirTodods(){
+        StringBuilder retorno = new StringBuilder("LISTA DE FUNCIONÁRIOS DA CONFEITARIA:<br><br>");
+        for (Funcionario f : listaFuncionario){
+            retorno.append(f.exibir());
+            retorno.append("<br>");
+        }
+        return retorno.toString();
+    }
+
+}
