43cbe88b09a1c6db52aec87bf26491020880423e
HelenaPetruz <helena.petruz@gmail.com>
2024-07-25 09:37:01 -0300

    atualização do repositorio do funcionario com hashmap

--- src/main/java/com/mentoria/helena/confeitaria/repository/FuncionarioRepository.java
+++ src/main/java/com/mentoria/helena/confeitaria/repository/FuncionarioRepository.java
@@ -4,38 +4,33 @@ import com.mentoria.helena.confeitaria.classes.Cliente;
 import com.mentoria.helena.confeitaria.classes.Funcionario;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class FuncionarioRepository implements IFuncionarioRepository{
     ArrayList<Funcionario> listaFuncionario = new ArrayList();
 
+    HashMap <Integer, Funcionario> lista = new HashMap<>();
+
     @Override
     public Funcionario get(int idFuncionario) {
-        for (Funcionario f: listaFuncionario){
-            if (f.getIdFuncionario()==idFuncionario){
-                return f;
-            }
-        }
-        return null;
+        return lista.get(idFuncionario);
     }
 
     @Override
     public void add(String nome, int idade, String telefone, String cpf, double salario) {
         Funcionario funcionario1 = new Funcionario(nome, idade, telefone, cpf, salario);
-        listaFuncionario.add(funcionario1);
+        lista.put(funcionario1.getIdFuncionario(), funcionario1);
     }
 
     @Override
     public void update(Funcionario funcionario) {
-
+        lista.put(funcionario.getIdFuncionario(), funcionario);
     }
 
     @Override
     public void remove(Funcionario funcionario) {
-        for (Funcionario f: listaFuncionario){
-            if (f==funcionario){
-                listaFuncionario.remove(f);
-            }
-        }
+        lista.remove(funcionario);
     }
+
     }
 
