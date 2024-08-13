c0cde1c4996789b2c0616d85981beb88694e338b
HelenaPetruz <helena.petruz@gmail.com>
2024-08-13 04:52:43 -0300

    arrumei nome do mapa e a classe Repository de Produto

--- src/main/java/com/mentoria/helena/confeitaria/repository/HashMapProdutoRepository.java
+++ src/main/java/com/mentoria/helena/confeitaria/repository/HashMapProdutoRepository.java
@@ -4,28 +4,28 @@ import com.mentoria.helena.confeitaria.classes.Produto;
 
 import java.util.HashMap;
 
-public class ProdutoRepository implements IProdutoRepository{
+public class HashMapProdutoRepository implements IProdutoRepository{
 
-    HashMap <Integer, Produto> lista = new HashMap<>();
+    HashMap <Integer, Produto> mapa = new HashMap<>();
 
     @Override
     public Produto get(int idProduto) {
-        return lista.get(idProduto);
+        return mapa.get(idProduto);
     }
 
     @Override
     public void add(String descricao, double preco, String categoria, String validade) {
         Produto produto = new Produto(descricao, preco, categoria, validade);
-        lista.put(produto.getId(), produto);
+        mapa.put(produto.getId(), produto);
     }
 
     @Override
     public void update(Produto produto) {
-        lista.put(produto.getIdProduto(), produto);
+        mapa.put(produto.getIdProduto(), produto);
     }
 
     @Override
     public void remove(Produto produto) {
-        lista.remove(produto);
+        mapa.remove(produto);
     }
 }
