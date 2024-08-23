3d23fee83d490359aa841369666ff1e47a330f08
HelenaPetruz <helena.petruz@gmail.com>
2024-08-22 21:02:54 -0300

    Ajustes no repositorio do funcionario

--- src/main/java/com/mentoria/helena/confeitaria/repository/HashMapFuncionarioRepository.java
+++ src/main/java/com/mentoria/helena/confeitaria/repository/HashMapFuncionarioRepository.java
@@ -1,12 +1,10 @@
 package com.mentoria.helena.confeitaria.repository;
 
-import com.mentoria.helena.confeitaria.classes.Cliente;
 import com.mentoria.helena.confeitaria.classes.Funcionario;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
-public class FuncionarioRepository implements IFuncionarioRepository{
+public class HashMapFuncionarioRepository implements IFuncionarioRepository{
 
     HashMap <Integer, Funcionario> mapa = new HashMap<>();
 
@@ -17,8 +15,8 @@ public class FuncionarioRepository implements IFuncionarioRepository{
 
     @Override
     public void add(String nome, int idade, String telefone, String cpf, double salario) {
-        Funcionario funcionario1 = new Funcionario(nome, idade, telefone, cpf, salario);
-        mapa.put(funcionario1.getIdFuncionario(), funcionario1);
+        Funcionario funcionario = new Funcionario(nome, idade, telefone, cpf, salario);
+        mapa.put(funcionario.getIdFuncionario(), funcionario);
     }
 
     @Override
