7005b6915c405f152416a1dcdd51bcaed2d87d61
Leonardo Andreatta de Alcantara <leonardo@hostfully.com>
2024-09-17 21:59:27 -0300

    fix: Endpoint de delete e criação e umas outras coisas

---
+++ src/main/java/com/mentoria/helena/confeitaria/controler/dto/ClienteDto.java
@@ -0,0 +1,14 @@
+package com.mentoria.helena.confeitaria.controler.dto;
+
+import lombok.Getter;
+import lombok.Value;
+
+@Value
+@Getter
+public class ClienteDto {
+    int idCliente;
+    String nome;
+    int idade;
+    String telefone;
+    String cpf;
+}
