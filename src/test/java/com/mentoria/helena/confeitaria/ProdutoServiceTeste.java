34316513349114ed22ace7e280369ff14f4d2eba
HelenaPetruz <helena.petruz@gmail.com>
2024-10-13 10:55:57 -0300

    teste de adicionar produto

---
+++ src/test/java/com/mentoria/helena/confeitaria/ProdutoServiceTeste.java
@@ -0,0 +1,36 @@
+package com.mentoria.helena.confeitaria;
+
+import com.mentoria.helena.confeitaria.classes.Produto;
+import com.mentoria.helena.confeitaria.repository.HashMapProdutoRepository;
+import com.mentoria.helena.confeitaria.repository.IProdutoRepository;
+import com.mentoria.helena.confeitaria.service.ProdutoService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ProdutoServiceTeste {
+
+    ProdutoService produtoService;
+    IProdutoRepository iProdutoRepository;
+
+    @BeforeEach
+    public void inicializar(){
+        iProdutoRepository = new HashMapProdutoRepository();
+        produtoService = new ProdutoService(iProdutoRepository);
+    }
+
+    @Test
+    public void adicionarProduto(){ //3A: Arrange, Act, Assert
+        //3A: arrange(ajeitar), criar os dados do teste
+        Produto produto = new Produto("Milkshake", 15, "Bebidas", "2024");
+
+        //Act(agir), chama a classe que vai testar
+        Produto p = produtoService.adicionarProduto(produto);
+
+        //Assert(verificar), verifica resultado da ação
+        Assertions.assertEquals(1, p.getIdProduto());
+    }
+
+
+
+}
