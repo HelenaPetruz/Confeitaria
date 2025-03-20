b7d16d4f7a2edd1b10173ba46316a90263948e9f
HelenaPetruz <helena.petruz@gmail.com>
2025-03-20 20:57:46 -0300

    get do bdProdutoRepository

--- src/main/java/com/mentoria/helena/confeitaria/repository/BdProdutoRepository.java
+++ src/main/java/com/mentoria/helena/confeitaria/repository/BdProdutoRepository.java
@@ -19,13 +19,27 @@ public class BdProdutoRepository implements IProdutoRepository{
     @Override
     public Produto get(int idProduto) {
         log.info("caiu no metodo do bd");
-        String sql = "select descricao from `confeitaria-produto` where idProduto=idProduto";
-        jdbcTemplate.execute(sql);
-        return null;
+        String sql = "select descricao from `confeitaria-produto` where idProduto=?";
+        //jdbcTemplate.execute(sql);
+
+        //Método queryForObject(sql, parametros da query, tipo de retorno) - para buscar um valor no bd
+        return jdbcTemplate.queryForObject(sql, new Object[]{idProduto}, (rs, rowNum) -> { //expressão lambda - não entendi direito
+            Produto produto = new Produto(
+                    rs.getString("descricao"),
+                    rs.getDouble("preco"),
+                    rs.getString("categoria"),
+                    rs.getTimestamp("validade").toString()
+            );
+            produto.setIdProduto(rs.getInt("idProduto"));
+            return produto;
+        });
     }
 
     @Override
     public Produto add(Produto produto) {
+        String sql = "insert into `confeitaria-produto` (descricao, preco, categoria, validade) values: (?, ?, ?, ?)";
+        //Método update - para INSERT, UPDATE e DELETE
+        //return jdbcTemplate.update(sql, new Object[]{produto.getDescricao(), produto.getPreco(), produto.getCategoria(), produto.getValidade()});
         return null;
     }
 
